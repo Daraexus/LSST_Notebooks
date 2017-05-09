544f151f98ac04d942191dc88e1d63d21bcde727
Juan Pablo Reyes Gomez <jpreyes@marcosmo.in2p3.fr>
2017-05-09 09:18:09 +0200

    Ligh curves found

--- Utils/DiaSourceTools.py
+++ Utils/DiaSourceTools.py
@@ -497,6 +497,45 @@ def build_lightcurve2(source_list, flux_parameter):
     return lightcurve
 
 
+def build_lightcurve3(source_list, flux_parameter):
+    """
+    Assemble a light curve data table from available files.
+    """
+
+    bandpasses = ['r']
+
+
+    lightcurve = {}
+    lightcurve['classification'] = []
+    lightcurve['bandpass'] = []
+    lightcurve['mjd'] = []
+    lightcurve['ra'] = []
+    lightcurve['dec'] = []
+    lightcurve['flux'] = []
+    lightcurve['flux_error'] = []
+    lightcurve['zp'] = []
+    lightcurve['zpsys'] = []
+    #lightcurve['ccd'] = []
+
+
+    for src in source_list:
+
+        #print 'yep',visit
+        lightcurve['classification'].append(src['classification_dipole'])
+        lightcurve['bandpass'].append(str('sdss' + bandpasses[0]))
+        
+        lightcurve['mjd'].append(src['mjd'])
+        lightcurve['ra'].append(src['coord_ra'])
+        lightcurve['dec'].append(src['coord_dec'])
+        lightcurve['flux'].append(src[flux_parameter])
+        lightcurve['flux_error'].append(src[flux_parameter+"Sigma"])
+        #lightcurve['flux'].append(src['base_CircularApertureFlux_12_0_flux'])
+        #lightcurve['flux_error'].append(src['base_CircularApertureFlux_12_0_fluxSigma'])
+        lightcurve['zp'].append(25.0)
+        lightcurve['zpsys'].append('ab')
+        #lightcurve['ccd'].append(src['ccd'])
+    lightcurve = Table(data=lightcurve)
+    return lightcurve
 
 def multi_match_catalogs(catalog_list, data_refs):
     multi_matches = None
